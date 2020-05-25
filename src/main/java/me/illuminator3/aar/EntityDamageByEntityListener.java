0b83d81152bb72931fcb122027ef3e9847c9cb3f
illuminator3 <48388251+illuminator3@users.noreply.github.com>
2020-05-25 12:21:10 +0200

    Better style

--- src/main/java/me/illuminator3/aar/EntityDamageByEntityListener.java
+++ src/main/java/me/illuminator3/aar/EntityDamageByEntityListener.java
@@ -5,7 +5,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -14,30 +16,79 @@ import org.bukkit.event.entity.EntityDamageEvent;
 public class EntityDamageByEntityListener
     implements Listener
 {
-    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent e)
     {
-        if (e.getCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK || !(e.getDamager() instanceof Player))
-            return;
+        if (!isValid(e)) return;
 
         Player p = (Player) e.getDamager();
+        double distance = calcDistance(p, e.getEntity());
 
-        if (p.getGameMode() == GameMode.CREATIVE)
-            return;
+        if (!isValid(distance, p)) return;
 
-        Location damagerLoc = p.getLocation(),
-                 entityLoc = e.getEntity().getLocation();
+        update(p);
+        notifyAll(p, distance);
+        cancel(e);
+    }
 
-        double distance;
+    protected boolean isValid(EntityDamageByEntityEvent e)
+    {
+        return e.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK && e.getDamager() instanceof Player && ((Player) e.getDamager()).getGameMode() != GameMode.CREATIVE;
+    }
 
-        distance = damagerLoc.distance(entityLoc);
+    protected boolean isValid(double distance, Player p)
+    {
+        return distance >= 4.8 + getLatency(p);
+    }
 
-        if (damagerLoc.getY() < entityLoc.getY())
-            distance -= (entityLoc.getY() - damagerLoc.getY()) / (2 + 10 / 3);
+    protected void cancel(Cancellable c)
+    {
+        c.setCancelled(true);
+    }
 
-        final double d = Utils.cut(distance, 1);
+    protected void notifyAll(Player p, double distance)
+    {
+        Bukkit.getOnlinePlayers().stream().filter(s -> s.hasPermission(AdvancedAntiReach.CONFIG.getString("permissions.notify"))).forEach(f -> AdvancedAntiReach.notify(f, p, distance));
+    }
 
+    protected void update(Player p)
+    {
+        if (updateLevel(p) == AdvancedAntiReach.MAX)
+        {
+            AdvancedAntiReach.LEVELS.remove(p.getUniqueId());
+
+            Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), ChatColor.translateAlternateColorCodes('&', AdvancedAntiReach.CONFIG.getString("command")).replace("%prefix%", AdvancedAntiReach.PREFIX).replace("%player%", p.getName()));
+        }
+    }
+
+    protected int updateLevel(Player p)
+    {
+        int current = 0;
+
+        if (AdvancedAntiReach.LEVELS.containsKey(p.getUniqueId()))
+            current = AdvancedAntiReach.LEVELS.get(p.getUniqueId());
+
+        AdvancedAntiReach.LEVELS.put(p.getUniqueId(), ++current);
+
+        return AdvancedAntiReach.LEVELS.get(p.getUniqueId());
+    }
+
+    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
+    protected double calcDistance(Entity ent1, Entity ent2)
+    {
+        Location ent1Loc = ent1.getLocation(),
+                 ent2Loc = ent2.getLocation();
+
+        double distance = ent1Loc.distance(ent2Loc);
+
+        if (ent1Loc.getY() < ent2Loc.getY())
+            distance -= (ent2Loc.getY() - ent1Loc.getY()) / (2 + 10 / 3);
+
+        return Utils.cut(distance, 1);
+    }
+
+    protected double getLatency(Player p)
+    {
         double latency = 0.;
         int ping = ((CraftPlayer) p).getHandle().ping;
 
@@ -45,25 +96,6 @@ public class EntityDamageByEntityListener
             latency = ((double) (ping)) / 100 / 10 * 1.1;
         /* else: ping spoof */
 
-        if (d >= (4.8 + latency))
-        {
-            int current = 0;
-
-            if (AdvancedAntiReach.LEVELS.containsKey(p.getUniqueId()))
-                current = AdvancedAntiReach.LEVELS.get(p.getUniqueId());
-
-            AdvancedAntiReach.LEVELS.put(p.getUniqueId(), ++current);
-
-            Bukkit.getOnlinePlayers().stream().filter(s -> s.hasPermission(AdvancedAntiReach.CONFIG.getString("permissions.notify"))).forEach(f -> AdvancedAntiReach.notify(f, p, d));
-
-            if (current == AdvancedAntiReach.MAX)
-            {
-                AdvancedAntiReach.LEVELS.remove(p.getUniqueId());
-
-                Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), ChatColor.translateAlternateColorCodes('&', AdvancedAntiReach.CONFIG.getString("command")).replace("%prefix%", AdvancedAntiReach.PREFIX).replace("%player%", p.getName()));
-            }
-
-            e.setCancelled(true);
-        }
+        return latency;
     }
 }
